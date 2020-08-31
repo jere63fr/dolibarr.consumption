81023feb91e210b13be0840adea554fe539da66f
jere63fr <37144301+jere63fr@users.noreply.github.com>
2020-08-31 09:40:43 +0200

    Update consumption.class.php
    replace print_titre by load_fiche_titre
    Correct Translation

--- consumption/class/consumption.class.php
+++ consumption/class/consumption.class.php
@@ -121,7 +121,7 @@ class Consumption extends CommonObject
 		if($right)
 		{
 				//form for consumption
-				print_titre("Consommation");
+				print load_fiche_titre($langs->trans("Consumption"));
 				//print "\"".$page.$_GET["id"]."\"";
 				print "<form action=\"".$page.$_GET["id"]."\" method=\"post\">\n";
 				print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
@@ -240,7 +240,7 @@ class Consumption extends CommonObject
 			'e.ref'=>array('label'=>$langs->trans("Warehouse"), 'checked'=>1, 'enabled'=>(! $id > 0)),	// If we are on specific warehouse, we hide it
 			'm.fk_user_author'=>array('label'=>$langs->trans("Author"), 'checked'=>0),
 			'm.inventorycode'=>array('label'=>$langs->trans("InventoryCodeShort"), 'checked'=>1),
-			'm.label'=>array('label'=>$langs->trans("LabelMovement"), 'checked'=>1),
+			'm.label'=>array('label'=>$langs->trans("MovementLabel"), 'checked'=>1),
 			'origin'=>array('label'=>$langs->trans("Origin"), 'checked'=>1),
 			'm.value'=>array('label'=>$langs->trans("Qty"), 'checked'=>1),
 		);
