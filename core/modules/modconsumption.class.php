8aecd68d055616535f48321aff35d6d2cd9fbc9b
jere63fr <37144301+jere63fr@users.noreply.github.com>
2020-12-22 11:56:39 +0100

    change version

--- core/modules/modconsumption.class.php
+++ core/modules/modconsumption.class.php
@@ -54,7 +54,7 @@ class modconsumption extends DolibarrModules
 		$this->editor_name = 'Jérémie TER-HEIDE';
 		$this->editor_url = 'http://ter-heide.fr';
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '3.5';
+		$this->version = '3.6';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
